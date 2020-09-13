4f4cd5ae75a9697d20b6e3a755645b020bd393bf
= <zainr7989@gmail.com>
2020-09-13 11:44:53 -0400

    Add controls to the emissions chart page.

--- web/app.py
+++ web/app.py
@@ -12,20 +12,38 @@ dfs = [
 app.emissions_df = dfs[0]
 # add a property for the different sources of the emissions
 app.carbon_categories = dfs[0]['Description'].unique()
+# add a propert for different colors, each for an emission category
+app.colors = [
+    '#F00064',  # hot pink
+    '#4BBCFE',  # light blue
+    '#F06400',  # orange
+    '#FF8DEF',  # pink
+    '#50296A',  # purple
+    '#810D0D',  # brown
+    '#545273',  # dark indigo
+    '#D8AA61',  # light brown
+    '#FFFDC7',  # yellow
+]
 
 
 # Route to Get the Home Page
 @app.route("/", methods=['GET'])
 def get_index():
     '''Return the view of the home page.'''
-    return render_template("index.html"), 200
+    return render_template("index.html"), 200  # template and response code
 
 
 # Route to Get Emission Page
 @app.route("/emissions", methods=['GET'])
 def get_emissions_chart():
     '''Return the view of the emissions chart page.'''
-    return render_template("emissions.html"), 200
+    return (
+        render_template(
+        "emissions.html",  # template name
+        categories=app.carbon_categories,
+        # colors=app.colors,  # context of template
+        ), 200  # response code
+    )
 
 
 if __name__ == "__main__":
