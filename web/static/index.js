be2a87aad5b4c547d69f12fff508b4df6d788418
= <zainr7989@gmail.com>
2020-09-17 22:36:10 -0400

    Fix togglers.

--- web/static/index.js
+++ web/static/index.js
@@ -15,111 +15,115 @@ const lines = [  // each is for a different energy source
 
 // Grab all the categories
 const categoryList = document.querySelector('.control-category ul');
-const columns = columnList.children;
+const columns = categoryList.children;
+
+for (let i = 0, length = columns.length; i < length; i += 1) {
+    columns[i].children[0].addEventListener("input", function () {
+        updateLines(columns);
+    });
+}
 
 // Update the lists to be checked - toggler
-columnList.addEventListener("click", evt => {
+categoryList.addEventListener("click", evt => {
     if (evt.target.tagName == "LI") {
         evt.target.classList.toggle("checked");
     }
 });
 
-let getChartData = (queryString = "?year=1973&year=2016&category=Total Energy Electric Power Sector CO2 Emissions") => {
+function getChartData(queryString = "?years=1973&years=2016&sources=Total Energy Electric Power Sector CO2 Emissions") {
     // sending the request and grabbing data
     axios
         .get("/time_series" + queryString)
         .then(res => {
             let timeSeries = res.data;
-            const seriesData = [];
             // list all categories of emissions
             const series = [
-                'Coal Electric Power Sector',
-                'Natural Gas Electric Power Sector',
-                'Distillate Fuel, Including Kerosene-Type Jet Fuel, Oil Electric Power Sector',
-                'Petroleum Coke Electric Power Sector',
-                'Residual Fuel Oil Electric Power Sector',
-                'Petroleum Electric Power Sector',
-                'Geothermal Energy Electric Power Sector',
-                'Non-Biomass Waste Electric Power Sector',
-                'Total Energy Electric Power Sector',
+                'Coal Electric Power Sector CO2 Emissions',
+                'Natural Gas Electric Power Sector CO2 Emissions',
+                'Distillate Fuel, Including Kerosene-Type Jet Fuel, Oil Electric Power Sector CO2 Emissions',
+                'Petroleum Coke Electric Power Sector CO2 Emissions',
+                'Residual Fuel Oil Electric Power Sector CO2 Emissions',
+                'Petroleum Electric Power Sector CO2 Emissions',
+                'Geothermal Energy Electric Power Sector CO2 Emissions',
+                'Non-Biomass Waste Electric Power Sector CO2 Emissions',
+                'Total Energy Electric Power Sector CO2 Emissions',
             ];
-            // Check if the current category exists
-            if (typeof timeSeries[currSeries] !== "undefined") {
-                const data = Object.values(timeSeries[currSeries]);
-                seriesData.push(data);
-            }
-        });
-    // create chart data
-    const chartData = {
-        labels: Object.tvalues(timeSeries.month).map(utc => {
-            const date = new Date(0);
-            date.setUTCMilliseconds(Number(utc));
-            return `${date.getFullYear()}-$date.getMonth()`};
-        }),
-        series: seriesData
-    };
-    chart.update(chartData);
-    updateLines(columnList.children);
-    // error handling
-    }).catch(err => console.log(err));
-}
+            const seriesData = [];
+            for (let i = 0, length = series.length; i < length; i += 1) {
+                const currentSeries = series[i];
 
-let updateLines = items => {
-    let currLine = 0;
-    // Grab all checked categories
-    for(let i = 0, length = items.length; i < length; i+= 1) {
-        currentLine = document.querySelector(
-            ".ct-series-" + lines[currLine] + " .ct-line"
-        );
-        currentPoints = document.querySelectorAll(
-            ".ct-series-" + lines[currLine] + " .ct-point"
-        );
-        currentLine.style.cssText = 
-        "stroke: " + items[i].children[0].value + "!important";
-        
-        // color every point
-        for (let j = 0, length = currentPoints.length; j < length; j += 1) {
-            currentPoints[j].style.cssText = 
-                "stroke: " + items[i].children[0].value + "!important";
-        }
-        currLine += 1;
-    }
+                // Ensure that the current series is within the time series.
+                if (typeof timeSeries[currentSeries] !== "undefined") {
+                    const data = Object.values(timeSeries[currentSeries]);
+                    seriesData.push(data);
+                }
+            }
+            console.log(timeSeries);
+            const chartData = {
+                labels: Object.values(timeSeries[0]).map(datetime => {
+                    const date = new Date(datetime);
+                    return `${date.getFullYear()}-${date.getMonth()}`;
+                }),
+                series: seriesData
+            };
+
+            // Update the chart and line colors.
+            chart.update(chartData);
+            updateLines(trendList.children);
+        })
+        .catch(err => console.error(err));
 }
 
 // updating the chart data
-let updateChart = () => {
+function updateChart() {
     let queryString;
-    const items = columnList.children;
 
-    // get both input fields from the time frame selection
+    // Get both input fields from the time frame section
     const timeFrames = document.querySelectorAll(".control-date > input");
 
-    // grab all the time ranges
-    for (let i = 0, length = timeFrames.legnth; i < length; i += 1) {
+    // Grab all time ranges
+    for (let i = 0, length = timeFrames.length; i < length; i += 1) {
         time = timeFrames[i].value;
 
         // Start or add to the query string
         if (typeof queryString === "undefined") {
-            queryString = "?year=" + time;
+            queryString = "?years=" + time;
         } else {
-            queryString += "&year=" + time;
+            queryString += "&years=" + time;
         }
     }
-    let currLine = 0;
-    // grab all checked columns 
-    for(let i = 0, length = items.length; i < length; i += 1) {
-        if (items[i].classList.contains("checked")) {
-            queryString += "category=" + items[i].innerText.toLowerCase();
+
+    // Grab all the checked columns
+    for (let i = 0, length = sources.length; i < length; i += 1) {
+        if (sources[i].classList.contains("checked")) {
+            queryString += "&sources=" + sources[i].innerText.toLowerCase();
         }
     }
-    // get the data related to the query
+
     getChartData(queryString);
-    // add event listeners to all the category color inputs, 
-    // so the lines are redrawn as data is updated
-    for (let i = 0, length = columns.length; i < length; i += 1) {
-        columns[i].children[0].addEventListener("input", () => {
-            updateLines(columns);
-        });
+}
+
+// Update the lines drawn with the correct colors
+function updateLines(items) {
+    let lineNumber = 0;
+    // Grab all the checked columns
+    for (let i = 0, length = items.length; i < length; i += 1) {
+        // Only color the checked off lines
+        if (items[i].classList.contains("checked")) {
+            currentLine =
+                document.querySelector(`.ct-series-${lines[lineNumber]} .ct-line`);
+            currentPoints =
+                document.querySelectorAll(`.ct-series-${lines[lineNumber]} .ct-point`);
+            currentLine.style.cssText =
+                `stroke: ${items[i].children[0].value} !important;`;
+
+            // Color every point
+            for (let j = 0, length = currentPoints.length; j < length; j += 1) {
+                currentPoints[j].style.cssText =
+                    `stroke: ${items[i].children[0].value} !important;`;
+            }
+            lineNumber += 1;
+        }
     }
 }
 
