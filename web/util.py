93a6c0208a5975d755cf1db572bcdf56ac60a10a
= <zainr7989@gmail.com>
2020-09-17 21:48:00 -0400

    Add function to remove NaN from DataFrame.

--- web/util.py
+++ web/util.py
@@ -2,9 +2,25 @@ import pandas as pd
 
 
 def clean_emission_data(path):
+    def fill_not_available(dataframe, column_name):
+        '''Fills in 0 for all values in the column that are populated by "Not Available."'''
+        new_values = list()
+        for item in dataframe[column_name].items():
+            # get the value to append
+            value = item
+            # if value is NaN, change to 0
+            if value == "Not Available":
+                value = 0
+            # add the value
+            new_values.append(value)
+        # reassign the column in the df
+        dataframe[column_name] = new_values
+        return dataframe
     """Converts all times in the emissions DataFrame into DateTime values."""
     # make a DF of the emissions CSV, wherever it's located
     carbon_df = pd.read_csv(path)
+    # store a list of the different sources
+    carbon_categories = carbon_df['Description'].unique()
     # convert dates to strings
     carbon_df['YYYYMM'] = [str(date)[:-2] + "-" + str(date)[-2:] for date in carbon_df['YYYYMM']]
     # remove all dates in the 13th month
@@ -13,7 +29,8 @@ def clean_emission_data(path):
     new_df["YYYYMM"] = pd.to_datetime(new_df['YYYYMM'], format="%Y-%m")
     # give each energy source it's own column
     new_df = new_df.pivot(index='YYYYMM', columns='Description', values='Value')
-    # TODO: remove NaN values
-    # store a list of the different sources
-    carbon_categories = dfs[0]['Description'].unique()
+    # remove NaN values
+    for category in carbon_categories:
+        new_df = fill_not_available(new_df, category)
+
     return new_df, carbon_categories
