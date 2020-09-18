2c53f9ffe5f7818ff06a1af8bf7b6904cea062b8
= <zainr7989@gmail.com>
2020-09-17 21:58:42 -0400

    Further DF slicing.

--- web/app.py
+++ web/app.py
@@ -64,20 +64,14 @@ def get_time_series_data():
 
     # Grab all of the data specified from start to stop range.
     selected_date_range = app.emissions_df[
-        (app.emissions_df["YYYYMM"] >= datetime.datetime(min_year, 1, 1)) &
-        (app.emissions_df["YYYYMM"] <= datetime.datetime(max_year, 12, 31))
+        (app.emissions_df.index >= datetime.datetime(min_year, 1, 1)) &
+        (app.emissions_df.index <= datetime.datetime(max_year, 12, 31))
     ]
 
-    # Slice the DF to include only the sources we want and then to sort our
-    # dataframe by those sources.
-    requested_sources_data = (
-        app.emissions_df.loc[
-            app.emissions_df['Description'] == 
-                'Total Energy Electric Power Sector CO2 Emissions', 
-            ['YYYYMM','Value']
-        ]
-    )
-    requested_trend_data = requested_trend_data.sort_values(by=["YYYYMM"])
+    # Slice the DF to include only the sources we want 
+    requested_trend_data = selected_date_range[sources]
+    # sort our dataframe by those sources.
+    # requested_trend_data = requested_trend_data.sort_values(by=["month"])
 
     # Return the dataframe as json
     return requested_trend_data.to_json(), 200
--- web/util.py
+++ web/util.py
@@ -27,10 +27,9 @@ def clean_emission_data(path):
     new_df = carbon_df[~carbon_df["YYYYMM"].str.contains('-13')]
     # convert to datetime
     new_df["YYYYMM"] = pd.to_datetime(new_df['YYYYMM'], format="%Y-%m")
-    # give each energy source it's own column
+    # give each energy source it's own column - now the date is the index
     new_df = new_df.pivot(index='YYYYMM', columns='Description', values='Value')
     # remove NaN values
     for category in carbon_categories:
         new_df = fill_not_available(new_df, category)
-
     return new_df, carbon_categories
