350d3bbd0158040eb2b4c2532164ec30653158db
= <zainr7989@gmail.com>
2020-09-22 18:14:18 -0400

    Fix web.util import, edit entry on Dockerfile.

--- web/app.py
+++ web/app.py
@@ -4,7 +4,7 @@ from functools import reduce
 from flask import Flask, render_template, request
 import pandas as pd
 
-import util
+import web.util as util
 
 # Instaniate global app variable
 app = Flask(__name__)
